aefc1d1a314908b02968912188736313873259c8
Philippe <philippe.rheaume.1@etsmtl.net>
2017-04-01 15:37:47 -0400

    Fix des avertissements de compilation et enlevé ce qui servait à rien.

--- LAB04/LAB04/Lab4.cpp
+++ LAB04/LAB04/Lab4.cpp
@@ -6,6 +6,26 @@
 #include <CL/cl.h>
 #include <windows.h>
 
+// *********************************************************************
+// Ce projet est basé sur l'exemple oclVectorAdd fait par NVIDIA recommandé
+// par le chargé de laboratoire.
+// *********************************************************************
+void *srcA, *srcB;        // Host buffers for OpenCL test
+cl_context cxGPUContext;        // OpenCL context
+cl_command_queue cqCommandQueue;// OpenCL command que
+cl_platform_id cpPlatform;      // OpenCL platform
+cl_device_id cdDevice;          // OpenCL device
+cl_program cpProgram;           // OpenCL program
+cl_kernel ckKernel;             // OpenCL kernel
+cl_mem buffA;               // OpenCL device source buffer A
+cl_mem buffB;                // OpenCL device destination buffer 
+size_t szGlobalWorkSize;        // 1D var for Total # of work items
+size_t szLocalWorkSize;		    // 1D var for # of work items in the work group	
+size_t szKernelLength;			// Byte size of kernel code
+cl_int ciErr1;			// Error code var
+char* cSourceCL = NULL;         // Buffer to hold source for compilation
+int m, n, np;
+double executionTimeSeq, executionTimePar, td, h;
 double PCFreq = 0.0;
 __int64 CounterStart = 0;
 
@@ -27,10 +47,9 @@ double GetCounter()
 	return double(li.QuadPart - CounterStart) / PCFreq;
 }
 
+//Code tiré de l'annexe fournie dans l'énoncé
 char* oclLoadProgSource(const char* cFilename, const char* cPreamble, size_t*szFinalLength)
 {
-
-	// locals
 	FILE* pFileStream = NULL;
 	size_t szSourceLength;
 
@@ -70,56 +89,6 @@ char* oclLoadProgSource(const char* cFilename, const char* cPreamble, size_t*szF
 	return cSourceString;
 }
 
-int roundUp(int numToRound, int multiple)
-{
-	if (multiple == 0)
-	{
-		return numToRound;
-	}
-
-	int roundDown = ((int)(numToRound) / multiple) * multiple;
-	int roundUp = roundDown + multiple;
-	int roundCalc = roundUp;
-	return (roundCalc);
-}
-
-// Name of the file with the source code for the computation kernel
-// *********************************************************************
-const char* cSourceFile = "Lab4.cl";
-
-// Host buffers for demo
-// *********************************************************************
-void *srcA, *srcB, *srcTDH, *srcM, *srcN;        // Host buffers for OpenCL test
-//float srcTDH;
-//int srcM, srcM;
-								// OpenCL Vars
-cl_context cxGPUContext;        // OpenCL context
-cl_command_queue cqCommandQueue;// OpenCL command que
-cl_platform_id cpPlatform;      // OpenCL platform
-cl_device_id cdDevice;          // OpenCL device
-cl_program cpProgram;           // OpenCL program
-cl_kernel ckKernel;             // OpenCL kernel
-cl_mem buffA;               // OpenCL device source buffer A
-cl_mem buffTDH;               // OpenCL device source buffer B 
-cl_mem buffM;               // OpenCL device source buffer A
-cl_mem buffN;               // OpenCL device source buffer B 
-cl_mem buffB;                // OpenCL device destination buffer 
-size_t szGlobalWorkSize;        // 1D var for Total # of work items
-size_t szLocalWorkSize;		    // 1D var for # of work items in the work group	
-size_t szParmDataBytes;			// Byte size of context information
-size_t szKernelLength;			// Byte size of kernel code
-cl_int ciErr1, ciErr2;			// Error code var
-char* cPathAndName = NULL;      // var for full paths to data, src, etc.
-char* cSourceCL = NULL;         // Buffer to hold source for compilation
-const char* cExecutableName = NULL;
-
-
-int err, m, n, np, nbproc;
-double timeStart1, timeStart2, timeEnd1, timeEnd2, executionTimeSeq, executionTimePar;
-double td, h;
-unsigned long tp;
-time_t t1, t2;
-
 int main(int argc, char *argv[])
 {
 
@@ -135,30 +104,29 @@ int main(int argc, char *argv[])
 	np = atoi(argv[3]); //nombre de pas de temps
 	td = atof(argv[4]); //le temps discretise
 	h = atof(argv[5]); //la taille d'un cote d'une subdivision
-	float tdh = td / (h*h);
+	float tdh = (float)td / (float)(h*h);
 	szLocalWorkSize = 256;
-	szGlobalWorkSize = m * n;  // rounded up to the nearest multiple of the LocalWorkSize
+	szGlobalWorkSize = m * n; // Taille de la matrice
 
-	#define M  m;
-	#define N  n;
-
-	//---------------------SEQUENTIELLE----------------------------------------------------------------------
+							  //---------------------SEQUENTIELLE----------------------------------------------------------------------
 	void *oldMatrix, *newMatrix;
 
+	//Allocation de la mémoire
 	oldMatrix = (void *)malloc(sizeof(cl_float) * szGlobalWorkSize);
 	newMatrix = (void *)malloc(sizeof(cl_float) * szGlobalWorkSize);
 
+	// Initialisation de la matrice
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			((float*)oldMatrix)[i + (j * n)] = i * (n - i - 1) * j * (m - j - 1);
-			((float*)newMatrix)[i + (j * n)] = i * (n - i - 1) * j * (m - j - 1);
+			((float*)oldMatrix)[i + (j * n)] = (float)i * (n - i - 1) * j * (m - j - 1);
+			((float*)newMatrix)[i + (j * n)] = (float)i * (n - i - 1) * j * (m - j - 1);
 		}
 	}
 
+	// Affichage de la matrice après initialisation
 	printf("%s\n", "Matrice apres initialisation");
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
@@ -172,7 +140,7 @@ int main(int argc, char *argv[])
 
 	for (int k = 0; k <= (np - 1); k++)
 	{
-		for (int i = 0; i < szGlobalWorkSize; i++)
+		for (int i = 0; i < m * n; i++)
 		{
 			if (i >= (m*n))
 			{
@@ -203,16 +171,14 @@ int main(int argc, char *argv[])
 			((float*)newMatrix)[i] = (1.0f - 4.0f * tdh) * ((float*)oldMatrix)[i] + tdh *
 				(((float*)oldMatrix)[i - 1] + ((float*)oldMatrix)[i + 1] + ((float*)oldMatrix)[i - n] + ((float*)oldMatrix)[i + n]);
 
-			
 		}
-
 		memmove(oldMatrix, newMatrix, sizeof(float) * szGlobalWorkSize);
 	}
 
 	executionTimeSeq = GetCounter();
 
+	// Affichage de la matrice après exécution séquentielle
 	printf("%s\n", "Sequentielle");
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
@@ -224,7 +190,7 @@ int main(int argc, char *argv[])
 	printf("\r\n");
 
 	//-------------------------------------------PARALLELE-----------------------------------------------------------------
-							   // Allocate and initialize host arrays 
+	// Allocation de la mémoire
 	srcA = (void *)malloc(sizeof(cl_float) * szGlobalWorkSize);
 	srcB = (void *)malloc(sizeof(cl_float) * szGlobalWorkSize);
 
@@ -232,50 +198,45 @@ int main(int argc, char *argv[])
 	{
 		for (int j = 0; j < m; j++)
 		{
-			((float*)srcA)[i + (j * n)] = i * (n - i - 1) * j * (m - j - 1);
+			((float*)srcA)[i + (j * n)] = (float)i * (n - i - 1) * j * (m - j - 1);
 		}
 	}
 
-	//Get an OpenCL platform
+	// Obtention de la plateforme OpenCL
 	ciErr1 = clGetPlatformIDs(1, &cpPlatform, NULL);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Erreur dans clGetPlatformID, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		getchar();
 	}
 
-	//Get the devices
+	// Obtention des devices (GPUs)
 	ciErr1 = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &cdDevice, NULL);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clGetDeviceIDs, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		getchar();
 	}
 
-	//Create the context
+	// Création du contexte
 	cxGPUContext = clCreateContext(0, 1, &cdDevice, NULL, NULL, &ciErr1);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clCreateContext, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		getchar();
 	}
 
-	// Create a command-queue
+	// Création de la commande
 	cqCommandQueue = clCreateCommandQueue(cxGPUContext, cdDevice, 0, &ciErr1);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clCreateCommandQueue, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		getchar();
 	}
 
-	// Allocate the OpenCL buffer memory objects for source and result on the device GMEM
+	// Création des bufffers dans le GPU
 	buffA = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr1);
 	buffB = clCreateBuffer(cxGPUContext, CL_MEM_WRITE_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr1);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clCreateBuffer, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
@@ -284,9 +245,8 @@ int main(int argc, char *argv[])
 
 	cSourceCL = oclLoadProgSource("Lab4.cl", "", &szKernelLength);
 
-	// Create the program
+	// Création du programme
 	cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSourceCL, &szKernelLength, &ciErr1);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clCreateProgramWithSource, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
@@ -294,46 +254,27 @@ int main(int argc, char *argv[])
 	}
 
 	ciErr1 = clBuildProgram(cpProgram, 0, NULL, NULL, NULL, NULL);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clBuildProgram, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
-
-		if (ciErr1 == CL_BUILD_PROGRAM_FAILURE) {
-			// Determine the size of the log
-			size_t log_size;
-			clGetProgramBuildInfo(cpProgram, cdDevice, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-
-			// Allocate memory for the log
-			char *log = (char *)malloc(log_size);
-
-			// Get the log
-			clGetProgramBuildInfo(cpProgram, cdDevice, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-
-			// Print the log
-			printf("%s\n", log);
-		}
-
 		getchar();
 	}
 
-	// Create the kernel
+	// Création du Kernel
 	ckKernel = clCreateKernel(cpProgram, "VectoraNextNP", &ciErr1);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clCreateKernel, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		getchar();
 	}
 
-	// Set the Argument values
+	// Définition des arguments passés au GPU
 	ciErr1 = clSetKernelArg(ckKernel, 0, sizeof(cl_mem), (void*)&buffA);
 	ciErr1 |= clSetKernelArg(ckKernel, 1, sizeof(cl_float), (void*)&tdh);
 	ciErr1 |= clSetKernelArg(ckKernel, 2, sizeof(cl_int), (void*)&n);
 	ciErr1 |= clSetKernelArg(ckKernel, 3, sizeof(cl_int), (void*)&m);
 	ciErr1 |= clSetKernelArg(ckKernel, 4, sizeof(cl_int), (void*)&np);
 	ciErr1 |= clSetKernelArg(ckKernel, 5, sizeof(cl_mem), (void*)&buffB);
-
 	if (ciErr1 != CL_SUCCESS)
 	{
 		printf("Error in clSetKernelArg, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
@@ -341,22 +282,19 @@ int main(int argc, char *argv[])
 	}
 
 	StartCounter();
-	
+
 	for (int k = 0; k <= (np - 1); k++)
 	{
-
-		// Asynchronous write of data to GPU device
+		// Envoie asynchrone des données au GPU
 		ciErr1 = clEnqueueWriteBuffer(cqCommandQueue, buffA, CL_FALSE, 0, sizeof(cl_float) * szGlobalWorkSize, srcA, 0, NULL, NULL);
 		ciErr1 |= clEnqueueWriteBuffer(cqCommandQueue, buffB, CL_FALSE, 0, sizeof(cl_float) * szGlobalWorkSize, srcB, 0, NULL, NULL);
-
 		if (ciErr1 != CL_SUCCESS)
 		{
 			printf("Error in clEnqueueWriteBuffer, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
 		}
 
-		// Launch kernel
+		// Appel du Kernel
 		ciErr1 = clEnqueueNDRangeKernel(cqCommandQueue, ckKernel, 1, NULL, &szGlobalWorkSize, NULL, 0, NULL, NULL);
-
 		if (ciErr1 != CL_SUCCESS)
 		{
 			int maxGlobSize = CL_DEVICE_MAX_WORK_GROUP_SIZE;
@@ -364,22 +302,16 @@ int main(int argc, char *argv[])
 			getchar();
 		}
 
-		// Synchronous/blocking read of results, and check accumulated errors
+		// Lecture synchrone du buffer
 		ciErr1 = clEnqueueReadBuffer(cqCommandQueue, buffB, CL_TRUE, 0, sizeof(cl_float) * szGlobalWorkSize, srcB, 0, NULL, NULL);
 
 		srcA = srcB;
 	}
-	
+
 	executionTimePar = GetCounter();
 
-	if (ciErr1 != CL_SUCCESS)
-	{
-		printf("Error in clEnqueueReadBuffer, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
-		getchar();
-	}
-
+	// Affichage de la matrice finale après exécution parallèle
 	printf("Parallele \r\n");
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
